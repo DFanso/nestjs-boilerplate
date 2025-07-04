d01d932cbe09ec3a83dde9a2ad56da3d69293481
Leo Gavin <71537398+DFanso@users.noreply.github.com>
2025-07-05 02:05:44 +0530

    Enhance application setup by integrating Swagger for API documentation, adding request logging, and implementing global validation pipes with improved error handling. Update package dependencies in package.json and pnpm-lock.yaml to include new libraries.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,50 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { useRequestLogging } from './utils/request-logging';
+import {
+  BadRequestException,
+  ValidationPipe,
+  VersioningType,
+} from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    rawBody: true,
+    bodyParser: true,
+  });
+  useRequestLogging(app);
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      exceptionFactory: (errors) => {
+        const messages = errors.map((error) => ({
+          property: error.property,
+          constraints: error.constraints,
+        }));
+        return new BadRequestException(messages);
+      },
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Boilerplate')
+    .setDescription(' Boilerplate')
+    .setVersion('1.0')
+    .addTag('Boilerplate')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+  app.enableCors();
+
+  await app.listen(9000);
 }
 bootstrap();
